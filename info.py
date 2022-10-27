91bb76ba0fde88ca0c6abf21f22d89be60b1377c
awdealofficial <awdealofficial@gmail.com>
2022-10-27 13:34:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID', '9411723'))
-API_HASH = environ('API_HASH', '30fa091455c0548d77dc254f0bb705b0')
-BOT_TOKEN = environ('BOT_TOKEN', '5792604981:AAFPd65l_192jCtMCpVnntrUqB5Vn1Au8jk')
+API_ID = int(environ.get('API_ID', '9411723'))
+API_HASH = environ.get('API_HASH', '30fa091455c0548d77dc254f0bb705b0')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5792604981:AAFPd65l_192jCtMCpVnntrUqB5Vn1Au8jk')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
