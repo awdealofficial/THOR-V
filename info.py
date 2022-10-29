1bbd3e422474de885ab5962a52492e9ab4d8600b
awdealofficial <awdealofficial@gmail.com>
2022-10-29 19:02:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://admin:admin@cluster0.hl4xqtk.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "My Database")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Mydatabase")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
